0336241f9865e5503f2aad038599bb4e39771b65
Edg3Runner <mitch.pauwels@proton.me>
2024-04-25 23:00:21 +0100

    Created Projectiles

--- index.js
+++ index.js
@@ -20,6 +20,29 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+  // Draw a circle
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
 // Place player in the middle of the screen
 const x = canvas.width / 2;
 const y = canvas.height / 2;
@@ -27,3 +50,26 @@ const y = canvas.height / 2;
 const player = new Player(x, y, 30, "blue");
 // Draw player on canvas
 player.draw();
+
+// Create projectiles
+const projectiles = [];
+
+function animate() {
+  requestAnimationFrame(animate);
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+}
+
+addEventListener("click", (event) => {
+  //   console.log("FIRE!!!");
+  //   console.log(event);
+  projectiles.push(
+    new Projectile(x, y, 5, "red", {
+      x: 1,
+      y: 1,
+    })
+  );
+
+  animate();
+});
